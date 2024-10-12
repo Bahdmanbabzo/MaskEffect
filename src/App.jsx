11e009b8d7aa484e0dffe9503bc9d96d8de143bb
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-12 08:10:30 +0100

    Extend tailwind to use external font.
    Tracking wider controls letter spacing.

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
         <motion.div className="h-full w-full bg-black border-8 border-amber-500">  </motion.div>
     </motion.section>
       <motion.section className="bg-[url('/lamboghini.jpg')] bg-cover bg-center h-full w-full absolute z-10">
-          <p className="font-extrabold text-5xl absolute left-0 top-3/4 text-amber-500"><span className="text-black">Built for speed</span>, with speed...</p>
+          <p className="font-extrabold text-5xl absolute left-0 top-3/4 text-amber-500 tracking-wider font-bangers"><span className="text-black">Built for speed</span>, with speed...</p>
       </motion.section>
   </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bangers:['Bangers', 'san-serif']
+      }
+    },
   },
   plugins: [],
 }
