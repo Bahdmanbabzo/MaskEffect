3e75d8ba3a8f247579aef382c79d83f5944e67bc
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-12 07:55:18 +0100

    Track mouse movements.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useAnimate,motion} from "framer-motion"
 import { useEffect, useState } from "react";
 
 function App() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  useEffect(()=> {
+    const handleMouseMove = (e) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    }
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, [])
   return (
     <div className="h-screen w-screen relative">
     <motion.section
